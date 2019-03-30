2a9422427a1ddbb7a8e391c584c421c8cc5f3555
Fiagdon9 <37011797+Fiagdon9@users.noreply.github.com>
2019-03-30 17:02:16 +0000

    Add files via upload

---
+++ js/active-menu.js
@@ -0,0 +1,8 @@
+$('.sf-menu li').removeClass('active');
+var path = window.location.pathname;
+$('.sf-menu li a').each(function() {
+	var href = $(this).attr('href');
+	if(path.slice(1).substring(0, href.length) === href) {
+		$(this).parent('li').addClass('active');
+	}
+});
